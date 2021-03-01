96af525524257a49cc0af108dbfad300df45fa11
EnesOZMEN <ozmen.enes@hotmail.com>
2021-03-01 23:41:23 +0100

    commit

---
+++ lib/screens/carousel/image_carousel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnnouncementSlider extends StatefulWidget {
+  @override
+  _AnnouncementSliderState createState() => _AnnouncementSliderState();
+}
+
+class _AnnouncementSliderState extends State<AnnouncementSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('AnnouncementSlider'),
+    );
+  }
+}
